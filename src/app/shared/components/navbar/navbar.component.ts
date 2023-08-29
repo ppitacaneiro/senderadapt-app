14f4d0415656a0cb36499a20bc574cd688c3e337
ppitacaneiro@gmail.com <ppitacaneiro@gmail.com>
2023-08-29 18:57:32 +0200

    back-button in navbar

--- src/app/shared/components/navbar/navbar.component.ts
+++ src/app/shared/components/navbar/navbar.component.ts
@@ -8,6 +8,7 @@ import { Component, Input, OnInit } from '@angular/core';
 export class NavbarComponent  implements OnInit {
 
   @Input() navBarText:string = "";
+  @Input() isVisibleBackButton:boolean = false;
 
   constructor() { }
 
