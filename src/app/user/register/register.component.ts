8d10fb59ef56404e0c8d474af2cd1b5a4baef92f
ppitacaneiro@gmail.com <ppitacaneiro@gmail.com>
2023-08-29 21:31:01 +0200

    validation register form

--- src/app/user/register/register.component.ts
+++ src/app/user/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -7,8 +8,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent  implements OnInit {
 
-  constructor() { }
+  constructor(private formBuilder:FormBuilder) { }
+  
+  registerForm = this.formBuilder.group({
+    name: ['', [Validators.required,Validators.minLength(8)]],
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required,Validators.minLength(8)]],
+    repeatPassword: ['', Validators.required,Validators.minLength(8)]
+  })
 
   ngOnInit() {}
+
+  onSubmit() {
+    console.log(this.registerForm.valid);
+    console.log('onSubmit',this.registerForm.value);
+  }
 
 }
