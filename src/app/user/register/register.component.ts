c1b85e0c697057a63c89968cb9314035b2b5dad1
ppitacaneiro@gmail.com <ppitacaneiro@gmail.com>
2023-08-31 18:57:20 +0200

    get and set form elements

--- src/app/user/register/register.component.ts
+++ src/app/user/register/register.component.ts
@@ -1,5 +1,7 @@
+import { UserService } from './../../services/user.service';
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
+import { User } from 'src/app/interfaces/user';
 
 @Component({
   selector: 'app-register',
@@ -8,20 +10,59 @@ import { FormBuilder, Validators } from '@angular/forms';
 })
 export class RegisterComponent  implements OnInit {
 
-  constructor(private formBuilder:FormBuilder) { }
+  repeatPassword:string = '';
+  user: User = {
+    name: '',
+    email: '',
+    password: ''
+  };
+
+  constructor(
+    private formBuilder:FormBuilder,
+    private userService:UserService
+  ) { }
   
   registerForm = this.formBuilder.group({
-    name: ['', [Validators.required,Validators.minLength(8)]],
-    email: ['', [Validators.required, Validators.email]],
-    password: ['', [Validators.required,Validators.minLength(8)]],
-    repeatPassword: ['', Validators.required,Validators.minLength(8)]
+    name: ['', [
+      Validators.required,
+      Validators.minLength(8)]
+    ],
+    email: ['', [
+      Validators.required, 
+      Validators.email]
+    ],
+    password: ['', [
+      Validators.required,
+      Validators.minLength(10)]
+    ],
+    repeatPassword: ['', [
+      Validators.required,
+      Validators.minLength(10)]
+    ]
   })
+
+  get name() { return this.registerForm.get('name'); }
+  get email() { return this.registerForm.get('email'); }
+  get password() { return this.registerForm.get('password'); }
 
   ngOnInit() {}
 
   onSubmit() {
-    console.log(this.registerForm.valid);
-    console.log('onSubmit',this.registerForm.value);
+    // console.log(this.registerForm.valid);
+    // console.log('onSubmit',this.registerForm.value);
+
+    console.log('onSubmit',this.user);
+
+    /*
+    this.userService.register(this.user).subscribe({
+      next: data => {
+        console.log('data',data);
+      },
+      error: error => {
+        console.log('error',error);
+      }
+    });
+    */
   }
 
 }
