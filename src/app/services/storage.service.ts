ac9c3e61cde376a1e385d55d1ecede193e8c53fc
ppitacaneiro@gmail.com <ppitacaneiro@gmail.com>
2023-10-18 20:19:48 +0200

    get and set methods for storage service

--- src/app/services/storage.service.ts
+++ src/app/services/storage.service.ts
@@ -1,9 +1,26 @@
 import { Injectable } from '@angular/core';
+import { Storage } from '@ionic/storage';
 
 @Injectable({
   providedIn: 'root'
 })
 export class StorageService {
+  private _storage: Storage | null = null;
 
-  constructor() { }
+  constructor(private storage:Storage) {
+    this.init();
+  }
+
+  async init(){
+    const storage = await this.storage.create();
+    this._storage = storage;
+  }
+
+  public async set(key: string, value: any){
+    await this._storage?.set(key, value);
+  }
+
+  public async get(key: string){
+    return await this._storage?.get(key);
+  }
 }
