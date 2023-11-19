ecf94a8341c4cfe7181cfd0d4012d41b6a58af6f
ppitacaneiro@gmail.com <ppitacaneiro@gmail.com>
2023-11-19 13:22:57 +0100

    custom input select

--- src/app/shared/components/input-select/input-select.component.ts
+++ src/app/shared/components/input-select/input-select.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-input-select',
@@ -7,8 +7,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InputSelectComponent  implements OnInit {
 
-  constructor() { }
+  @Input() label: string = '';
+  @Input() valueOption: string = '';
+  @Input() labelOption: string = '';
+  @Input() options: any[] = [];
+  @Input() placeholder: string = '';
 
-  ngOnInit() {}
+  constructor() {}
+
+  ngOnInit() {
+    this.buildOptions();
+  }
+
+  buildOptions() {
+    console.log('options',this.options);
+    this.options = this.options.map((option) => {
+      return {
+        value: option[this.valueOption],
+        label: option[this.labelOption],
+      };
+    });
+    console.log('options2',this.options);
+  }
 
 }
