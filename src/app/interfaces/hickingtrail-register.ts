b053d6ee7c97f84a15eee65ee2144e8d2e73472f
Pablo Pita <ppitacaneiro@gmail.com>
2024-02-03 12:55:41 +0100

    nullable properties

--- src/app/interfaces/hickingtrail-register.ts
+++ src/app/interfaces/hickingtrail-register.ts
@@ -1,18 +1,19 @@
 import { Step } from "./step";
 
 export interface HickingtrailRegister {
+    uuid?: string;
     user_id: number;
-    distance_kms: number;
-    time_minutes: number;
+    distance_kms?: number;
+    time_minutes?: number;
     community_id: number;
     province_id: number;
     municipality_id: number;
     origin_name: string;
-    origin_lat: number;
-    origin_lng: number;
+    origin_lat?: number;
+    origin_lng?: number;
     destination_name: string;
-    destination_lat: number;
-    destination_lng: number;
+    destination_lat?: number;
+    destination_lng?: number;
     difficulty_level: string;
-    steps: Step[];
+    steps?: Step[];
 }
