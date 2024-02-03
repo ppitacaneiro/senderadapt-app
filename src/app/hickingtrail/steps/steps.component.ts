9b5823c86a27aada318f10a328a8d5d5899f3d5f
Pablo Pita <ppitacaneiro@gmail.com>
2024-02-03 14:10:16 +0100

    calculata steps and send data to server

--- src/app/hickingtrail/steps/steps.component.ts
+++ src/app/hickingtrail/steps/steps.component.ts
@@ -1,11 +1,14 @@
 import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { AnimationController, IonCard } from '@ionic/angular';
 import { Animation } from '@ionic/angular';
-import { Geolocation } from '@capacitor/geolocation';
+import { Geolocation, Position } from '@capacitor/geolocation';
 import { StorageService } from 'src/app/services/storage.service';
-import { ActivatedRoute, Route, Router } from '@angular/router';
-import { Hickingtrail } from 'src/app/interfaces/hickingtrail';
+import { ActivatedRoute, Router } from '@angular/router';
 import { HickingtrailRegister } from 'src/app/interfaces/hickingtrail-register';
+import { Subscription, interval } from 'rxjs';
+import { Step } from 'src/app/interfaces/step';
+import { HickingtrailService } from 'src/app/services/hickingtrail.service';
+import { ToastService } from 'src/app/services/toast.service';
 
 @Component({
   selector: 'app-steps',
@@ -15,31 +18,42 @@ import { HickingtrailRegister } from 'src/app/interfaces/hickingtrail-register';
 export class StepsComponent implements OnInit {
   @ViewChild(IonCard, { read: ElementRef }) card!: ElementRef<HTMLIonCardElement>;
 
+  steps: Step[] = [];
+  startDate?:Date;
+  endDate?:Date;
+  timeInMinutes?:number;
   uuid?: string;
   private animation!: Animation;
   isPlaying = false;
   hickingtrail!: HickingtrailRegister;
+  watchPosition!: Subscription;
 
   constructor(
     private animationCtrl: AnimationController,
     private storageService: StorageService,
     private route: ActivatedRoute,
+    private router: Router,
+    private toastService: ToastService,
+    private hickingtrailService: HickingtrailService
   ) {}
 
-  async getCurrentPosition() {
-    const coordinates = await Geolocation.getCurrentPosition();
-    this.storageService.get('hickingtrail').then((response) => {
-      const actualHickingtrail = [response] as HickingtrailRegister[];
-      this.hickingtrail = actualHickingtrail.find((h: HickingtrailRegister) => h.uuid === this.uuid)!;
+  setCurrentPosition(coordinates: Position) {
+    this.storageService.get('hickingtrails').then((hickintrails:HickingtrailRegister[]) => {
+      this.hickingtrail = hickintrails.find((h: HickingtrailRegister) => h.uuid === this.uuid)!;
       this.hickingtrail.origin_lat = coordinates.coords.latitude;
       this.hickingtrail.origin_lng = coordinates.coords.longitude;
+      this.storageService.set('hickingtrails', hickintrails);
     });
-    this.storageService.set('hickingtrail', this.hickingtrail);
+  }
+
+  async getOriginPosition() {
+    const coordinates = await Geolocation.getCurrentPosition();
+    this.setCurrentPosition(coordinates);
   }
 
   ngOnInit() {
     this.uuid = this.route.snapshot.paramMap.get('uuid')!;
-    this.getCurrentPosition();
+    this.getOriginPosition();
   }
 
   ngAfterViewInit() {
@@ -52,12 +66,59 @@ export class StepsComponent implements OnInit {
   }
 
   play() {
+    this.startDate = new Date();
     this.animation.play();
     this.isPlaying = true;
+    this.getSteps();
   }
 
   stop() {
+    this.endDate = new Date();
+    this.timeInMinutes = (this.endDate.getTime() - this.startDate!.getTime()) / 60000;
     this.animation.stop();
+    this.watchPosition.unsubscribe();
+    this.save();
   }
 
+  async save() {
+    const coordinates = await Geolocation.getCurrentPosition();
+    this.storageService.get('hickingtrails').then((hickintrails:HickingtrailRegister[]) => {
+      this.hickingtrail = hickintrails.find((h: HickingtrailRegister) => h.uuid === this.uuid)!;
+      this.hickingtrail.time_minutes = this.timeInMinutes!;
+      this.hickingtrail.destination_lat = coordinates.coords.latitude;
+      this.hickingtrail.destination_lng = coordinates.coords.longitude;
+      this.hickingtrail.steps = this.steps;
+      this.hickingtrail.distance_kms = 0;
+      this.storageService.set('hickingtrails', hickintrails).then(() => {
+        // TODO: check internet connection
+        this.sendToServer(this.hickingtrail);
+      });
+    });
+  }
+
+  sendToServer(hickingtrail: HickingtrailRegister) {
+    this.hickingtrailService.saveHickingTrail(hickingtrail).subscribe({
+      next: () => {
+        this.toastService.presentToast('Ruta guardada correctamente');
+        this.router.navigate(['/hickingtrail/search']);
+      },
+      error: (error) => {
+        console.log('error guardando ruta', error);
+      }
+    });
+  }
+
+  getSteps() {
+    // TODO: change to watchPosition when testing on a real device
+    this.watchPosition = interval(5000).subscribe(() => {
+      Geolocation.getCurrentPosition().then((coordinates) => {
+        console.log('coordinates', coordinates);
+        this.steps.push({
+          lat: coordinates.coords.latitude,
+          lng: coordinates.coords.longitude,
+        } as Step);
+      });
+    });  
+
+  }
 }
